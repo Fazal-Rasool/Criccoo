533e97b684e27d442d819424b04327ff59294580
Fazal e Rasool <fazalrasool789@gmail.com>
2019-04-24 16:27:23 +0500

    Google Sign in

--- app/src/main/java/com/adaxiom/criccoo/Login.java
+++ app/src/main/java/com/adaxiom/criccoo/Login.java
@@ -1,10 +1,12 @@
 package com.adaxiom.criccoo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +23,13 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,7 +43,11 @@ public class Login extends AppCompatActivity {
     private CallbackManager callbackManager;
     private static final String EMAIL = "email";
 
-    private TextView tvName, tvEmail;
+    private SignInButton signInButton;
+    private GoogleSignInClient mGoogleSigninClient;
+    private int RQ_SIGN_IN=0;
+
+    private TextView tvName, tvEmail, tvNameGoogle, tvEmailGoogle;
     private ImageView ivProfile;
 
     @Override
@@ -43,15 +56,30 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        tvName = (TextView) findViewById(R.id.tvName);
-        tvEmail = (TextView) findViewById(R.id.tvEmail);
-        ivProfile = (ImageView) findViewById(R.id.ivProfileImage);
-        loginButton = (LoginButton) findViewById(R.id.login_button);
+        tvName = findViewById(R.id.tvName);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvNameGoogle = findViewById(R.id.tvNameGoogle);
+        tvEmailGoogle = findViewById(R.id.tvEmailGoogle);
+        ivProfile = findViewById(R.id.ivProfileImage);
+        loginButton = findViewById(R.id.login_button);
+        signInButton = findViewById(R.id.signInGoogle);
 
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSigninClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                googleSignIn();
+            }
+        });
 
 
         callbackManager = CallbackManager.Factory.create();
-        loginButton.setReadPermissions(Arrays.asList("email","public_profile"));
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
         checkLoginStatus();
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -74,55 +102,99 @@ public class Login extends AppCompatActivity {
 
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        callbackManager.onActivityResult(requestCode,resultCode,data);
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onStart() {
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null){
+            Toast.makeText(Login.this, "Google Already sign in", Toast.LENGTH_SHORT).show();
+            displayGoogleSignInData(account);
+        }
+        super.onStart();
     }
+
+    private void googleSignIn(){
+        Intent signInIntent = mGoogleSigninClient.getSignInIntent();
+        startActivityForResult(signInIntent, RQ_SIGN_IN);
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask){
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            displayGoogleSignInData(account);
+        }catch (ApiException e){
+            Log.e("Google Exception ", e.getStatusMessage());
+        }
+    }
+
+
+    private void displayGoogleSignInData(GoogleSignInAccount account){
+        if(account != null){
+            String name = account.getDisplayName();
+            String givenName = account.getGivenName();
+            String familyName = account.getFamilyName();
+            String email = account.getEmail();
+            String id = account.getId();
+            Uri photoUrl = account.getPhotoUrl();
+
+            tvNameGoogle.setText(name +" "+givenName+" "+familyName);
+            tvEmailGoogle.setText(email +" "+id);
+
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RQ_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+
+
 
     AccessTokenTracker tokenTracker = new AccessTokenTracker() {
         @Override
-        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
-        {
-            if(currentAccessToken==null)
-            {
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null) {
                 tvName.setText("");
                 tvEmail.setText("");
                 ivProfile.setImageResource(0);
-                Toast.makeText(Login.this,"User Logged out",Toast.LENGTH_LONG).show();
-            }
-            else
+                Toast.makeText(Login.this, "User Logged out", Toast.LENGTH_LONG).show();
+            } else
                 loadUserProfile(currentAccessToken);
         }
     };
 
 
-    private void loadUserProfile(AccessToken newAccessToken)
-    {
+    private void loadUserProfile(AccessToken newAccessToken) {
         GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
             @Override
-            public void onCompleted(JSONObject object, GraphResponse response)
-            {
+            public void onCompleted(JSONObject object, GraphResponse response) {
                 try {
                     String first_name = object.getString("first_name");
                     String last_name = object.getString("last_name");
 //                    String email = object.getString("email");
                     String id = object.getString("id");
-                    String image_url = "https://graph.facebook.com/"+id+ "/picture?type=normal";
+                    String image_url = "https://graph.facebook.com/" + id + "/picture?type=normal";
 
                     tvEmail.setText("");
-                    tvName.setText(first_name +" "+last_name);
+                    tvName.setText(first_name + " " + last_name);
                     RequestOptions requestOptions = new RequestOptions();
                     requestOptions.dontAnimate();
 
                     Glide.with(Login.this).load(image_url).into(ivProfile);
 
 
-                    if(!first_name.equalsIgnoreCase("")){
+                    if (!first_name.equalsIgnoreCase("")) {
                         startActivity(new Intent(Login.this, MainActivity.class));
-                    }else{
+                    } else {
                         Toast.makeText(Login.this, "Login failed", Toast.LENGTH_SHORT).show();
                     }
-
 
 
                 } catch (JSONException e) {
@@ -133,19 +205,15 @@ public class Login extends AppCompatActivity {
         });
 
         Bundle parameters = new Bundle();
-        parameters.putString("fields","id,name,link,email,first_name,last_name,gender");
+        parameters.putString("fields", "id,name,link,email,first_name,last_name,gender");
         request.setParameters(parameters);
         request.executeAsync();
 
     }
 
 
-
-
-    private void checkLoginStatus()
-    {
-        if(AccessToken.getCurrentAccessToken()!=null)
-        {
+    private void checkLoginStatus() {
+        if (AccessToken.getCurrentAccessToken() != null) {
             loadUserProfile(AccessToken.getCurrentAccessToken());
         }
     }
